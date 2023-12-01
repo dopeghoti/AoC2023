8d100eda38b1c2426e1e64fc9f3087bb91caaa01
Rob Palkowski <dopeghoti@gmail.com>
2023-12-01 11:02:09 +0000

    Refactor and optimization
    Use more comprehensions; use a single function for finding the first
    and last "digit or numberword" match.  It ѕtill will accept an input
    line that has only one match and use that match twice, though.
    
    I suspect that will be tomorrow's puzzle.

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
 FILE = '1.in'
-
 data = [ _.strip() for _ in open(f'inputs/{FILE}').readlines() ]
+
 digits = (
         '0',
         '1',
@@ -23,52 +23,29 @@ digits = (
         'eight',
         'nine' )
 values = {
-        'one': 1,
-        'two': 2,
-        'three': 3,
-        'four': 4,
-        'five': 5,
-        'six': 6,
-        'seven': 7,
-        'eight': 8,
-        'nine': 9 }
+        'one':   '1',
+        'two':   '2',
+        'three': '3',
+        'four':  '4',
+        'five':  '5',
+        'six':   '6',
+        'seven': '7',
+        'eight': '8',
+        'nine':  '9' }
 
-def fmatch( s: str):
-    """Find the first digit or numberword in a string"""
-    indices = { digit: 0 for digit in digits }
-    for digit in indices:
-        indices[digit] = s.find(digit)
-    nulls = []
-    for digit in indices:
-        if indices[digit] == -1:
-            nulls.append(digit)
-    for _ in nulls:
-        indices.pop(_)
-    first = min( indices, key = indices.get )
-    if first.isnumeric():
-        return first
-    else:
-        return values[first]
+def match_nums( s: str ) -> tuple:
+    """Find the first and last digit or numberword in a string"""
+    indices = { digit: s.find(digit) for digit in digits if s.find( digit ) != -1 }
+    first = min( indices, key = indices.get, default = None )
+    last = max( indices, key = indices.get, default = None )
+    if any( _ is None for _ in ( first, last ) ):
+        raise ValueError( f'Input string did not have a match: {s}' )
+    if first.isalpha():
+        first=values[first]
+    if last.isalpha():
+        last=values[last]
+    return ( int(first), int(last) )
 
-def lmatch( s: str):
-    """Find the last digit or numberword in a string"""
-    indices = { digit[::-1]: 0 for digit in digits }
-    for digit in indices:
-        indices[digit] = s[::-1].find(digit)
-    nulls = []
-    for digit in indices:
-        if indices[digit] == -1:
-            nulls.append(digit)
-    for _ in nulls:
-        indices.pop(_)
-    last = min( indices, key = indices.get )
-    if last.isnumeric():
-        return last
-    else:
-        return values[last[::-1]]
-
-calibration_values = []
-for _ in data:
-    calibration_values.append( int( f'{fmatch( _ )}{lmatch( _ )}' ) )
+calibration_values = [ 10 * first + last for (first, last) in ( match_nums(_) for _ in data ) ]
 print( f"Total of calibration values is {sum( calibration_values ) }." )
 
